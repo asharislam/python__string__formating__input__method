cf809b2af9b591344dc90dad6224adfd7a46edac
Ashar Islam <55947821+asharislam@users.noreply.github.com>
2022-05-12 23:32:22 +0600

    Add files via upload

---
+++ string_formating.py
@@ -0,0 +1,18 @@
+######### string formating ##########
+"""full_info = "name:{0}, village: {1}, age: {2}".format("ashar islam", "dhamghar", "21")
+print(full_info)"""
+###########
+"""name = input("please enter your name: ")
+village = input("please enter your village: ")
+age = input("please enter your age: ")
+full_bio = f"Name: {name}, village: {village}, Age: {age}"
+print(full_bio)
+"""
+##################
+full_name = {
+    "name": "ashar islam",
+    "proffetion": "student",
+    "village": "Dhamghar",
+    "zilla": "comilla",
+    "up-zilla": "muradnaghar"
+}
